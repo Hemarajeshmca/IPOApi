b822a34b89f353ee81ff997711cdf3f450c090fa
venkatesh-2280 <m.venkatesh@flexicodeindia.com>
2026-03-28 13:51:37 +0530

    BOA Screen

---
+++ Controllers/BOAController.cs
@@ -0,0 +1,35 @@
+﻿using IPOApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace IPOApi.Controllers
+{
+    public class BOAController : Controller
+    {
+        private IConfiguration _configuration;
+        public BOAController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        string constring = "";
+
+        [HttpGet("getboalist")]
+        public IActionResult getboalist(string offer_code)
+        {
+            constring = _configuration.GetSection("Appsettings")["ConnectionStrings"].ToString();
+            DataTable response = new DataTable();
+            try
+            {
+                response = BOAService.GetBoaService(offer_code, constring);
+                var serializedProduct = JsonConvert.SerializeObject(response, Formatting.None);
+                return Ok(serializedProduct);
+            }
+            catch (Exception e)
+            {
+                return Problem(title: e.Message);
+            }
+
+        }
+    }
+}
---
+++ Services/BOAService.cs
@@ -0,0 +1,24 @@
+﻿using IPOApi.Models;
+using IPOApi.STADataAccess;
+using System.Data;
+
+namespace IPOApi.Services
+{
+    public class BOAService
+    {
+        public static DataTable GetBoaService(string offer_code, string constring)
+        {
+            DataTable ds = new DataTable();
+            try
+            {
+                BOAData objDS = new BOAData();
+                ds = objDS.GetBoaData(offer_code, constring);
+            }
+            catch (Exception e)
+            { }
+            return ds;        }
+
+      
+
+    }
+}
