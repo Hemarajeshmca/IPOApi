0325b5986b6f4742f22d4cda3a9c2e401f28a9d8
venkatesh-2280 <m.venkatesh@flexicodeindia.com>
2026-04-02 18:02:27 +0530

    PAN Validation

---
+++ Services/PanValidateService.cs
@@ -0,0 +1,24 @@
+﻿using IPOApi.Models;
+using IPOApi.STADataAccess;
+using System.Data;
+
+namespace IPOApi.Services
+{
+    public class PanValidateService
+    {
+        public static DataTable PanValidService(string offer_code, string constring)
+        {
+            DataTable ds = new DataTable();
+            try
+            {
+                PanValidateData objDS = new PanValidateData();
+                ds = objDS.panvaliddata(offer_code, constring);
+            }
+            catch (Exception e)
+            { }
+            return ds;        }
+
+      
+
+    }
+}
