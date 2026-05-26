4dfafcd64b41be8430db5f3e1562212b4092b8e6
Hemarajeshmca <hema@flexicodeindia.com>
2026-05-26 18:58:48 +0530

    Hema changes

--- Controllers/EmailController.cs
+++ Controllers/EmailController.cs
@@ -38,6 +38,39 @@ namespace IPOApi.Controllers
             }
         }
 
+        [HttpGet("getemailList")]
+        public IActionResult getemailList(string offer_code)
+        {
+            DataSet response = new DataSet();
+            constring = _configuration.GetSection("Appsettings")["ConnectionStrings"].ToString();
+            try
+            {
+                response = _service.getemailListService(offer_code, constring);
+                var serializedProduct = JsonConvert.SerializeObject(response, Formatting.None);
+                return Ok(serializedProduct);
+            }
+            catch (Exception e)
+            {
+                return Problem(title: e.Message);
+            }
+        }
+
+        [HttpGet("Getbidfilecountsummary")]
+        public IActionResult Getbidfilecountsummary(string ipo_code)
+        {
+            constring = _configuration.GetSection("Appsettings")["ConnectionStrings"].ToString();
+            DataSet response = new DataSet();
+            try
+            {               
+                response = _service.GetbidfilecountsummaryService(ipo_code, constring);
+                var serializedProduct = JsonConvert.SerializeObject(response, Formatting.None);
+                return Ok(serializedProduct);
+            }
+            catch (Exception e)
+            {
+                return Problem(title: e.Message);
+            }
+        }
 
     }
 }
