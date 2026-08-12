103c8970142d898e4eed19324a891d785e893237
MangaiPalraj <mangai@flexicodeindia.com>
2026-08-12 14:33:59 +0530

    Configuration API

---
+++ Services/ConfigurationService.cs
@@ -0,0 +1,34 @@
+﻿using IPOApi.STADataAccess;
+using System.Data;
+
+namespace IPOApi.Services
+{
+    public class ConfigurationService
+    {
+        public static DataTable fetchconfigservice(string constring)
+        {
+            DataTable ds = new DataTable();
+
+            try
+            {
+                ConfigurationData objDS = new ConfigurationData();
+
+                ds = objDS.fetchconfigData(constring);
+            }
+            catch (Exception e)
+            {
+            }
+
+            return ds;
+        }
+
+        public static DataTable UpdateConfig(string json,int userId,string constring)
+        {
+            ConfigurationData objData = new ConfigurationData();
+            return objData.UpdateConfig(
+                json,
+                userId,
+                constring);
+        }
+    }
+}
