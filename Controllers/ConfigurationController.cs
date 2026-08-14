9417b8c3790a16a736d3640482cc04386d1249de
venkatesh-2280 <m.venkatesh@flexicodeindia.com>
2026-08-14 10:43:05 +0530

    Configuration API Screens

---
+++ Controllers/ConfigurationController.cs
@@ -0,0 +1,65 @@
+﻿using IPOApi.Models;
+using IPOApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace IPOApi.Controllers
+{
+    //[Route("[controller]")]
+    public class ConfigurationController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public ConfigurationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        string constring = "";
+
+        [HttpPost("fetchconfig")]
+        public IActionResult fetchconfig()
+        {
+            DataTable response = new DataTable();
+
+            try
+            {
+                constring = _configuration
+                    .GetSection("Appsettings")["ConnectionStrings"]
+                    .ToString();
+
+                response = ConfigurationService.fetchconfigservice(constring);
+
+                var serializedProduct = JsonConvert.SerializeObject(
+                    response,
+                    Formatting.None
+                );
+
+                return Ok(serializedProduct);
+            }
+            catch (Exception e)
+            {
+                return Problem(title: e.Message);
+            }
+        }
+
+        [HttpPost("updateconfig")]
+        public IActionResult UpdateConfig([FromBody] ConfigurationModel objConfig)
+        {
+            try
+            {
+                constring = _configuration.GetSection("Appsettings")["ConnectionStrings"].ToString();
+                string json = JsonConvert.SerializeObject(objConfig);
+                //string userId = Request.Headers["user_id"].FirstOrDefault();
+                DataTable response = ConfigurationService.UpdateConfig(json,constring);
+                var serializedProduct =JsonConvert.SerializeObject(response, Formatting.None);
+                return Ok(serializedProduct);
+            }
+            catch (Exception e)
+            {
+                return Problem(title: e.Message);
+            }
+        }
+    }
+}
---
+++ Services/ConfigurationService.cs
@@ -0,0 +1,33 @@
+﻿using IPOApi.STADataAccess;
+using System.Data;
+
+namespace IPOApi.Services
+{
+    public class ConfigurationService
+    {
+        public static DataTable fetchconfigservice(string constring)
+        {
+            DataTable ds = new DataTable();
+
+            try
+            {
+                ConfigurationData objDS = new ConfigurationData();
+
+                ds = objDS.fetchconfigData(constring);
+            }
+            catch (Exception e)
+            {
+            }
+
+            return ds;
+        }
+
+        public static DataTable UpdateConfig(string json,string constring)
+        {
+            ConfigurationData objData = new ConfigurationData();
+            return objData.UpdateConfig(
+                json,                
+                constring);
+        }
+    }
+}
